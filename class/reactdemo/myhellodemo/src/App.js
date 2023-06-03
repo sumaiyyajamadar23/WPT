10e4626be6173e872e15011bbb6b0d45304f7c88
dac <sumaiyyajamadar23@gmail.com>
2023-06-03 16:28:42 +0530

    react first

---
+++ class/reactdemo/myhellodemo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Myheader from './components/Myheader';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Header</h1>
+      <Myheader></Myheader>
+     <h1>My name is sumaiyya</h1>
+     <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
---
+++ class/reactdemo/myhellodemo/src/components/Myheader.js
@@ -0,0 +1,13 @@
+var Myheader=()=>
+{
+    var name="Sumaiyya"
+    return(
+        <div>
+            <h2>Hello from this Side ^._.^</h2>
+            <h3>Im {name}</h3>
+            <h3>Welcome to react programming</h3>
+        </div>
+    )
+    
+}
+export default Myheader;
